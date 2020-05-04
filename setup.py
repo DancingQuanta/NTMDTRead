243bfd018661ec5792f1f07d23c4bb2677219834
DancingQuanta <andytheseeker@gmail.com>
2020-05-04 12:17:21 +0100

    Using simple setup.py to install this package for testing and dev

--- setup.py
+++ setup.py
@@ -1,5 +1,19 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from setuptools import setup
 
-if __name__ == "__main__":
-	setup()
+requirements = [
+	'pint'
+]
+
+setup(
+    name='NTMDTRead',
+    version='0.1.0',
+    packages=['NTMDTRead'],
+    install_requires=requirements,
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ])
